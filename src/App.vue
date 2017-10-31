3c854fc70e9324b6532f8d85f3d0e73f10a423d6
Livio <livio.brunner.lb1@gmail.com>
2017-10-31 16:35:31 +0100

    Improve schochi galvanik card

--- src/App.vue
+++ src/App.vue
@@ -11,4 +11,7 @@ export default {
 </script>
 
 <style>
+body {
+  background: #181818;
+}
 </style>
--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,15 +1,20 @@
 <template>
   <main>
       <div class="row center-xs middle-xs">
-          <div class="schochi-galvanik row center-xs middle-xs">
-              <div class="box row center-xs middle-xs" data-tilt>
-                  <div class="column">
-                      <h2>SCHOCHI</h2>
-                      <div class="divider"></div>
-                      <h2>GALVANIK</h2>
-                  </div>
-               </div>
-          </div>
+          <article>
+            <div class="date">
+                <span class="month">JANUARY</span> Fri 12
+            </div>
+            <div class="schochi-galvanik row center-xs middle-xs">
+                <div class="box row center-xs middle-xs" data-tilt>
+                    <div class="column inner-box">
+                        <h2>SCHOCHI</h2>
+                        <div class="divider"></div>
+                        <h2>GALVANIK</h2>
+                    </div>
+                </div>
+            </div>
+          </article>
       </div>
   </main>
 </template>
@@ -20,10 +25,8 @@ export default {
   name: 'Home',
   mounted() {
     VanillaTilt.init(document.querySelector('.schochi-galvanik'), {
-      max: 25,
+      max: 15,
       speed: 400,
-      glare: true,
-      'max-glare': 0.2,
     });
   },
 };
@@ -41,16 +44,43 @@ $articleWidth: 400px;
   transform-style: preserve-3d;
 }
 
+
+
+main {
+    padding-top: 20px;
+}
+
+article {
+    .date {
+        margin-bottom: 20px;
+        font-family: 'Raleway', sans-serif;
+        color: white;
+        font-size: 25px;
+        .month {
+            color: #7A7A7A;
+            font-size: 20px;
+        }
+    }
+}
+
 .divider {
     height: 3px; 
-    width: 140%;
-    margin-left: -20%;
-    background: black;
+    width: 120%;
+    margin-left: -10%;
+    background: #181818;
 }
 .box {
-    border: 3px solid black;
-    width: $articleHeight - 50;
-    height: $articleWidth - 50;
-    transform: translateZ(20px);
+    border: 3px solid #181818;
+    width: $articleHeight - 60;
+    height: $articleWidth - 60;
+    transform: translateZ(40px);
+    .inner-box {
+        h2 {
+            font-family: 'Playfair Display', serif;
+            font-size: 40px;
+            margin: 10px;
+            color: #181818;
+        }
+    }
 }
 </style>
