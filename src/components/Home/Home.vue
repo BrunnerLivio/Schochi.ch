982298aa8ca30d44c4db59d8f60e7f77b175f12e
Livio <livio.brunner.lb1@gmail.com>
2017-11-06 16:16:37 +0100

    Add fingerprint

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,25 +1,30 @@
 <template>
-  <main class="wrap container-fluid">
-    <div>
-      <h2 class="upcoming-events">
-        <span class="light">Upcoming</span>
-        <span class="dark"> Event</span>
-      </h2>
-    </div>
-    <div class="row center-xs middle-xs">
-      <article>
-        <router-link to="january2018">
-          <div class="date">
-            <span class="dark">JANUARY</span>
-            <span class="light">FRI 12</span>
-          </div>
-        </router-link>
-        <router-link to="january2018">
-          <jan2018></jan2018>
-        </router-link>
-      </article>
-    </div>
-  </main>
+  <div class="column">
+    <main class="wrap container-fluid">
+        <div>
+          <h2 class="upcoming-events">
+            <span class="light">Upcoming</span>
+            <span class="dark"> Event</span>
+          </h2>
+        </div>
+        <div class="row center-xs middle-xs">
+          <article>
+            <router-link to="january2018">
+              <div class="date">
+                <span class="dark">JANUARY</span>
+                <span class="light">FRI 12</span>
+              </div>
+            </router-link>
+            <router-link to="january2018">
+              <jan2018></jan2018>
+            </router-link>
+          </article>
+        </div>
+      </main>
+      <footer>
+        <span class="light">website by </span> <a class="dark" href="https://brunnerliv.io" target="_blank">livio brunner</a>
+      </footer>
+  </div>
 </template>
 <script>
 import JAN2018 from '../Events/JAN2018/JAN2018';
@@ -34,8 +39,21 @@ export default {
 
 
 <style lang="scss" scoped>
+$footer-height : 80px;
 main {
     padding-top: 20px;
+    min-height: calc(100vh - #{$footer-height});
+}
+
+footer {
+  width: 100%;
+  text-align: center;
+  font-family: 'Roboto Mono', monospace;
+  color: #7A7A7A;
+  margin-top: 20px;
+  a{
+    color: #7A7A7A;
+  }
 }
 
 .upcoming-events {
