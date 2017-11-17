3a138d3c5740933104478edb789fa6e414515667
Livio Brunner <livio.brunner.lb1@gmail.com>
2017-11-17 09:24:23 +0100

    Add hash to mail

--- bin/generate-mail.js
+++ bin/generate-mail.js
@@ -2,14 +2,19 @@ const hbs = require('handlebars');
 const fs = require('fs');
 const moment = require('moment');
 const process = require('process');
-
+const crypto = require('crypto');
 fs.readFile('src/mail/template.hbs', 'utf8', (err, data) => {
     if (err) {
         return console.log(err);
     }
     const result = hbs.compile(data);
+
+    var current_date = (new Date()).valueOf().toString();
+    var random = Math.random().toString();
+    
     console.log(result({
         name: process.argv[3],
-        date: moment().format("DD.MM.YY")
+        date: moment().format("DD.MM.YY"),
+        hash: crypto.createHash('sha1').update(current_date + random).digest('hex')
     }));
 });
