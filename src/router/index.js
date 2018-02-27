fda23e0a73081fd51a3fa3c846c7f0ed40a0dd90
Livio Brunner <livio.brunner.lb1@gmail.com>
2018-02-27 10:29:02 +0100

    Add 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Home from '@/components/Home/Home';
 import JAN2018Detail from '@/components/Events/JAN2018/JAN2018-detail';
 import Router from 'vue-router';
 import Vue from 'vue';
+import fourzerofour from '@/components/404/404';
 
 Vue.use(Router);
 
@@ -16,6 +17,15 @@ export default new Router({
       path: '/january2018',
       name: 'JAN2018',
       component: JAN2018Detail,
+    },
+    {
+      path: '/404',
+      name: '404',
+      component: fourzerofour,
+    },
+    {
+      path: '*',
+      component: fourzerofour,
     },
   ],
 });
